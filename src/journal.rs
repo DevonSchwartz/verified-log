73e7ecb159a173c08ac37c5c5913ad7967e06b7c
Devon Schwartz <devon.s.schwartz@gmail.com>
2025-09-12 12:50:45 -0500

    Creating a journal with default values as shallow copies of the default value

--- src/journal.rs
+++ src/journal.rs
@@ -1,16 +1,15 @@
 use vstd::prelude::*;
-use std::marker::Copy; 
 
 verus!{
 
-pub struct Journal<T : Clone, const N: usize> {
+pub struct Journal<T : Clone + Copy, const N: usize> {
     data: [T; N], // this is the main data storage
     tx_buffer: Vec<(usize, T)>, // this is the transaction buffer, storing the index and the new value
     log: Vec<Vec<(usize, T)>>
 }
 
 
-impl<T : Clone, const N: usize> View for Journal<T, N> {
+impl<T : Clone + Copy, const N: usize> View for Journal<T, N> {
 
     type V = Seq<T>;
 
@@ -23,12 +22,14 @@ impl<T : Clone, const N: usize> View for Journal<T, N> {
 }
 
 impl<T : Clone + Copy, const N : usize> Journal<T, N> {
-    pub fn new(default_value : T) -> (out: Self)
+    // We can't use T::default() because it is not supported by Verus, so we need to use a default value
+    pub fn new(default_value:T) -> (out: Self)
         ensures
             out@.len() == N,
+            forall|i : int | 0 <= i < out@.len() ==> #[trigger] out@[i] == default_value
         {
             Self {
-                data: [default_value.clone(); N],
+                data: [default_value; N], // eventually, we want this to be a clone
                 log: Vec::new(),
                 tx_buffer: Vec::new()
             }
