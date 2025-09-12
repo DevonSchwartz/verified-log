cb8f6011e099d9ddaa4951f212ca3eb6d0ed5778
Devon Schwartz <devon.s.schwartz@gmail.com>
2025-09-12 11:51:30 -0500

    Failing post condition forall elements initialized to default value

--- src/journal.rs
+++ src/journal.rs
@@ -1,24 +1,35 @@
 use vstd::prelude::*;
+use std::marker::Copy; 
 
 verus!{
 
-pub struct Journal<T : Clone> {
-    data: Vec<T>, // this is the main data storage
+pub struct Journal<T : Clone, const N: usize> {
+    data: [T; N], // this is the main data storage
     tx_buffer: Vec<(usize, T)>, // this is the transaction buffer, storing the index and the new value
     log: Vec<Vec<(usize, T)>>
 }
 
-impl 
 
-impl<T : Clone> Journal<T> {
-    pub fn new(num_blocks: usize, default_value : T) -> (out: Self)
+impl<T : Clone, const N: usize> View for Journal<T, N> {
+
+    type V = Seq<T>;
+
+    // the view of the journal should be a sequence of its data
+    closed spec fn view(&self) -> Seq<T>
+        decreases self,
+    {
+        self.data@
+    }
+}
+
+impl<T : Clone + Copy, const N : usize> Journal<T, N> {
+    pub fn new(default_value : T) -> (out: Self)
         ensures
-            out.data@.len() == num_blocks,
-            forall|i : int | 0 <= i < out.data@.len() ==> #[trigger] out.data@[i] == default_value
-            
+            out@.len() == N,
+            forall|i : int | 0 <= i < out@.len() ==> #[trigger] out@[i] == default_value
         {
             Self {
-                data: vec![default_value; num_blocks],
+                data: [default_value.clone(); N],
                 log: Vec::new(),
                 tx_buffer: Vec::new()
             }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ verus! {
 use journal::*;
 
 fn main() {
-    println!("Hello, world!");
 }
 
 } // verus!
