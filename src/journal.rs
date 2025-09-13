10ebe37b7fa9849fd35cb85671b1e2ac38004b2a
Devon Schwartz <devon.s.schwartz@gmail.com>
2025-09-12 21:50:16 -0500

    Created filesystem and journal impl

--- src/journal.rs
+++ src/journal.rs
@@ -7,6 +7,7 @@ pub struct Filesystem<T: Copy, const N: usize>
     filesystem: [T; N]
 }
 
+// The view of the filesystem reduces to the view of its internal container
 impl<T: Copy, const N: usize> View for Filesystem<T, N>
 {
 
@@ -54,7 +55,8 @@ pub struct Journal<T: Copy>
 
 impl<T: Copy> View for Journal <T>
 {
-    type V = Seq<(usize, T)>;
+    // We use (usize,T) so that we can produce a sequence of this tuple. V has to match the return type
+    type V = Seq<(usize, T)>; 
 
     // the view of the journal should be a sequence of its data
     closed spec fn view(&self) -> Seq<(usize, T)>
@@ -75,52 +77,5 @@ impl <T: Copy> Journal<T>
                 is_transaction : false
             }
         }
-
-     
-
 }
-
-
-
-
-
-
-// pub struct Journal<T: Copy, const N: usize> {
-//     // TODO: Remove data and tx_buffer. Only keep log
-//     // TODO: Add a variable for the tail index, which says where the next piece of transaction should be written 
-//     // TODO: Use a static array for journal first, initialized to size client wants
-//     // TODO: Log is a one-to-one mapping of the data
-//     data: [T; N], // this is the main data storage
-//     tx_buffer: Vec<(usize, T)>, // this is the transaction buffer, storing the index and the new value
-//     log: Vec<Vec<(usize, T)>>, // After a
-//     // TODO: Add an enum for state 
-// }
-
-
-// impl<T: Copy, const N: usize> View for Journal<T, N> {
-
-//     type V = Seq<T>;
-
-//     // the view of the journal should be a sequence of its data
-//     closed spec fn view(&self) -> Seq<T>
-//     {
-//         self.data@
-//     }
-// }
-
-// impl<T: Copy, const N : usize> Journal<T, N> {
-//     // We can't use T::default() because it is not supported by Verus, so we need to use a default value
-//     pub fn new(&self, default_value:T) -> (out: Self)
-//         ensures
-//             out@.len() == N,
-//             forall|i : int | 0 <= i < out@.len() ==> #[trigger] out@[i] == default_value,            
-//         {
-//             Self {
-//                 data: [default_value; N], // eventually, we want this to be a clone
-//                 log: Vec::new(),
-//                 tx_buffer: Vec::new()
-//             }
-//         }
-
-// }
 }
