2626fceb7a9e1d8cf2dc38e5403cec652794602a
Devon Schwartz <devon.s.schwartz@gmail.com>
2025-11-20 09:16:31 -0600

    Using subrange works for all cases except invariant

--- src/journal.rs
+++ src/journal.rs
@@ -118,13 +118,20 @@ impl <T: Copy, const N : usize> Journal<T, N>
             0 <= old(self).last_checkpoint <= old(self).last_commit <= old(self).write_ptr <= N,
         ensures
             0 <= self.last_checkpoint <= self.last_commit == self.write_ptr <= N,
-            forall |i : int| old(self).last_checkpoint as int <= i < self.last_checkpoint ==> #[trigger]
-                self@[i] == self.filesystem@[i]
+            self@.subrange(old(self).last_checkpoint as int, self.last_checkpoint as int) 
+                == self.filesystem@.subrange(old(self).last_checkpoint as int, self.last_checkpoint as int)
         {
             self.last_commit = self.write_ptr;
             self.checkpoint();
         }
 
+    // pub proof fn lemma_seq_forall(s1: Seq<T>, s2: Seq<T>, i : int, j:int)
+    //     requires
+    //         0 <= i < j <= s1.len() == s2.len(),
+    //         forall|x : int| i <= x < j ==> #[trigger] s1[x] == s2[x]
+    //     {
+    //         assert(forall|x : int| i <= x < j ==> #[trigger] s1[x] == s2[x])
+    //     }
     
     fn checkpoint(&mut self)
         requires
@@ -134,8 +141,11 @@ impl <T: Copy, const N : usize> Journal<T, N>
             old(self).last_commit == self.last_commit,
             0 <= old(self).last_checkpoint <= self.last_checkpoint == self.last_commit <= self.filesystem@.len(),
             self@ == old(self)@,
-            forall |i : int| old(self).last_checkpoint as int <= i < self.last_checkpoint ==> #[trigger]
-                self@[i] == self.filesystem@[i]
+            // Self::lemma_seq_forall(self@, self.filesystem@, old(self).last_checkpoint, self.last_checkpoint),  
+            // forall |i : int| old(self).last_checkpoint as int <= i < self.last_checkpoint ==> #[trigger]
+            //     self@[i] == self.filesystem@[i]
+            self@.subrange(old(self).last_checkpoint as int, self.last_checkpoint as int) 
+                == self.filesystem@.subrange(old(self).last_checkpoint as int, self.last_checkpoint as int)
     {
         while self.last_checkpoint < self.last_commit
             invariant
